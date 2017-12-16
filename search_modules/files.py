eef6d1957c2e6ba4e52db582f1ebe4779db7472a
Jonathan Simmonds <jonathansimmonds@gmail.com>
2017-12-16 19:53:28 +0000

    Add very rough template for files module

--- search_modules/files.py
+++ search_modules/files.py
@@ -1,6 +1,28 @@
 # (c) Copyright 2017 Jonathan Simmonds
-"""Search module for searching file contents."""
+"""Search module for searching file contents.
+
+This will ignore .git and .svn directories.
+"""
+import subprocess
+
+# Module version
 __version__ = '1.0'
+
+def grep(regex, paths, ignore_case):
+    # r (recursive), n (print line num), s (no error messages), H (print filename),
+    # Z (NUL terminate filenames), I (ignore binary files), P (Perl regex)
+    grep_args = '-rnsHZIP'
+    if ignore_case:
+        grep_args += 'i'
+    try:
+        return subprocess.check_output(['grep', '--color=never', grep_args,
+                                        '--exclude-dir=".svn"',
+                                        '--exclude-dir=".git"', regex] + paths).strip()
+    except subprocess.CalledProcessError as cpe:
+        if cpe.returncode == 1:
+            return ''
+        else:
+            raise
 
 def search(regex, paths, command_args, ignore_case=False, verbose=False):
     """Perform the requested search.
@@ -14,7 +36,10 @@ def search(regex, paths, command_args, ignore_case=False, verbose=False):
             False if it should be case-sensitive.
         verbose:        Boolean, True for verbose output, False otherwise.
     """
-    print 'grep search_result'
+    results = grep(regex, paths, ignore_case)
+    # TODO prettify output
+    if results:
+        print results
 
 def create_subparser(subparsers):
     """Creates this module's subparser.
